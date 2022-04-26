ff239fbd2a88967e980bc96d6a28a030f013f772
Márcio <email.marcioalves@gmail.com>
2022-04-26 01:14:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_ou_impar
+{
+    class Program
+    {
+       static void Main(string[] args)
+        { 
+            int num;
+
+            Console.WriteLine("Escreva um número:");
+            num= int.Parse(Console.ReadLine());
+            parOuImpar(num);
+        }
+        static void parOuImpar(int num1)
+        {
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Este número é par");
+            }
+            else
+            {
+                Console.WriteLine("Este número é impar");
+            }
+        }
+    }
+}
